922ac16d35c8b22661f58941b77486c55d022e3c
evelinrm <eramos@devbot.com>
2020-04-22 23:57:15 -0500

    ultimos cambios

---
+++ src/iConditionals/QueHago.java
@@ -0,0 +1,38 @@
+package iConditionals;
+
+public class QueHago {
+    public static void main(String[] args){
+//        int vigenciaNetflix;
+//        double dineroCuenta;
+//
+//        if(dineroCuenta>2000.00){
+//            irAlCine();
+//        } else if(vigenciaNetflix > 0){
+//            invitar("Novia");
+//            netflixAndChill();
+//        } else{
+//            jugarVideoJuegos();
+//            estudiar("selenium");
+//        }
+    }
+
+    private static void irAlCine() {
+    }
+
+    private static void estudiar(String materia) {
+    }
+
+    private static void jugarVideoJuegos() {
+    }
+
+    private static void invitar(String persona){
+        
+    }
+    
+    private static void netflixAndChill(){
+        
+    }
+    
+    
+    
+}
---
+++ src/iLoops/cancionElefantes.java
@@ -0,0 +1,23 @@
+package iLoops;
+
+import libs.Input;
+
+public class cancionElefantes {
+    public static void main(String[] args){
+        for(int num = 1; num <= 100 ; num += 1) {
+            if (num == 1) {
+                System.out.println(num + " elefante se columpiaba sobre la tela de una araña,\n" +
+                        "como veia que resistia, fue a llamar a otro elefante");
+            } else  {
+                System.out.println(num + " elefantes se columpiaban sobre la tela de una araña,\n" +
+                        "como veian que resistia, fueron a llamar a otro elefante");
+            }
+
+        }
+    }
+    //Equipo
+    //Evelin
+    //Luisa
+    //Guillermo
+    //Daniela
+}
