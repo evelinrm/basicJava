d9f63b34179e07913aa15888acc9bb3d20aaf635
Evelin Ramos <eramos@devbot.com>
2020-07-30 10:15:10 -0500

    try arrays

---
+++ src/mArrays/RemoverCharacter.java
@@ -0,0 +1,17 @@
+package mArrays;
+
+import libs.Input;
+
+public class RemoverCharacter {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Cadena: ");
+		String c = Input.get_string(); 
+		
+		
+		
+
+	}
+
+}
